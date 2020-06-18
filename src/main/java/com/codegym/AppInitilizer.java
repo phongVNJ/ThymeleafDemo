6fab2b7609f47e36c4904be419a6c74750babea5
= <=>
2020-06-18 16:00:24 +0700

    add

---
+++ src/main/java/com/codegym/AppInitilizer.java
@@ -0,0 +1,39 @@
+package com.codegym;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.FilterRegistration;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+
+public class AppInitilizer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{AppConfiguration.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        FilterRegistration.Dynamic filterRegistration =
+                servletContext.addFilter("endcoding-filter", new CharacterEncodingFilter());
+        filterRegistration.setInitParameter("encoding", "UTF-8");
+        filterRegistration.setInitParameter("forceEncoding", "true");
+
+        //make sure encodingFilter is matched most first, by "false" arg
+        filterRegistration.addMappingForUrlPatterns(null, false, "/*");
+
+        super.onStartup(servletContext);
+    }
+}
